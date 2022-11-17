2a8aec92e975df5b866636f32bb96254363540ce
Mhd Ali Houria <alihoria123456@gmail.com>
2022-11-17 13:34:19 +0200

    Creating and styling Error page

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Error() {
   return (
-    <div>Error</div>
+    <div className='error-container'>
+      <h1 className="error-code strong-text">404 - Not Found</h1>
+      <h3 className="error-desc regular-text">
+        The content you are looking for has been removed or unavailable. 
+      </h3>
+      <h4 className="error-request thin-text">
+      Please check your info and try again later.
+      </h4>
+    </div>
   )
 }
 
